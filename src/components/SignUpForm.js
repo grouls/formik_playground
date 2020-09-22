26a5fca69373dc819eae4c95b502edbe3d0b265e
Gary Roulstone <47697948+grouls@users.noreply.github.com>
2020-09-22 15:09:16 +0100

    Add material ui tidy up with material ui & add additional fields

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,26 +1,62 @@
 import React from "react";
 import { useFormik } from "formik";
+import Button from "@material-ui/core/Button";
+import { Container, Grid, TextField } from "@material-ui/core";
 
 const SignupForm = () => {
   const formik = useFormik({
-    initialValues: { email: "" },
+    initialValues: { forename: "", surname: "", email: "" },
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     }
   });
-  const { handleSubmit, handleChange, values } = formik;
+  const { handleSubmit, handleChange } = formik;
+  const { forename, surname, email } = formik.values;
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={handleChange}
-        value={values.email}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <Container component="main" maxWidth="xs">
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              label="Forename"
+              id="forename"
+              name="forename"
+              type="text"
+              onChange={handleChange}
+              value={forename}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Surname"
+              id="surname"
+              name="surname"
+              type="text"
+              onChange={handleChange}
+              value={surname}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              onChange={handleChange}
+              value={email}
+            />
+          </Grid>
+        </Grid>
+        <Button
+          className="button"
+          type="submit"
+          variant="contained"
+          color="primary"
+        >
+          Submit
+        </Button>
+      </form>
+    </Container>
   );
 };
 
